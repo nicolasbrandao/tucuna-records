af1eae9a3065db933d2d466fcde6871672cc7a48
Nícolas Brandão <nicolas.brandaor@gmail.com>
2023-05-28 23:30:23 -0300

    fix: change layout language

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
-import './globals.css'
 import { Inter } from 'next/font/google'
+import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
       <body className={inter.className}>{children}</body>
     </html>
   )
