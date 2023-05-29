26bcfb0968fdb9bce094a014e3ab64158f080152
Nícolas Brandão <nicolas.brandaor@gmail.com>
2023-05-29 10:59:52 -0300

    fea: protect /app route

---
+++ app/app/layout.tsx
@@ -0,0 +1,24 @@
+import authOptions, { getOriginPath, makeRedirectURL } from '@/shared/auth'
+import { redirect } from 'next/navigation'
+import { getServerSession } from 'next-auth'
+import AuthProvider from '../contexts/AuthProvider'
+
+export const metadata = {
+  title: 'Tucuna Records',
+  description: 'Homologue suas conquistas',
+}
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const session = await getServerSession(authOptions)
+
+  if (!session) {
+    const origin = getOriginPath()
+    return redirect(makeRedirectURL(origin))
+  }
+
+  return <AuthProvider>{children}</AuthProvider>
+}
--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,7 +1,14 @@
+'use client'
+
+import { signOut } from 'next-auth/react'
+
 export default function Home() {
   return (
     <main className="bg-midnight-blue h-full">
       <p className="text-cerulean">APP TUCUNA RECORDS</p>
+      <button type="button" onClick={() => signOut()}>
+        SIGN OUT
+      </button>
     </main>
   )
 }
