8b6e5055491bb3855a7e8c974c52ba992ede4276
Nícolas Brandão <nicolas.brandaor@gmail.com>
2023-10-30 17:55:23 -0300

    REF: remove old components

--- app/app/recordes/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Records() {
-  return (
-    <main className="bg-midnight-blue h-screen">
-      <p>Página de Recordes</p>
-    </main>
-  )
-}
--- app/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import LoginForm from '../components/LoginForm'
-
-export default function Login() {
-  return (
-    <main className="bg-midnight-blue h-screen">
-      <LoginForm />
-    </main>
-  )
-}
