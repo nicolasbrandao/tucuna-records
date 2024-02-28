e81158f18609ad21472ad57d93a08432e75d3694
Nícolas Brandão <nicolas.brandaor@gmail.com>
2024-02-28 19:01:21 -0300

    REF: refactor the whole project

--- app/api/auth/[...nextauth]/route.ts
+++
@@ -1,6 +0,0 @@
-import NextAuth from 'next-auth/next'
-import { authOptions } from '@/shared/auth'
-
-const handler = NextAuth(authOptions)
-
-export { handler as GET, handler as POST }
--- app/contexts/Providers.tsx
+++
@@ -1,8 +0,0 @@
-import { PropsWithChildren } from 'react'
-import AuthProvider from './AuthProvider'
-
-function Providers({ children }: PropsWithChildren) {
-  return <AuthProvider>{children}</AuthProvider>
-}
-
-export default Providers
